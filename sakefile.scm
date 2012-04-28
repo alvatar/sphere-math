bfabb348c18ab8281156baa54875e5d1830662e2
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-04-28 19:58:18 +0200

    using Sake for tasks

---
+++ sakefile.scm
@@ -0,0 +1,24 @@
+(define lib-directory "lib/")
+(define lib-name "math")
+(define lib-suffix ".o1")
+
+(define-task init ()
+  (make-directory (current-build-directory)))
+
+(define-task clean (init)
+  (delete-file (current-build-directory))
+  (delete-file lib-directory))
+
+(define-task compile (init)
+  (gambit-compile-file
+   "module.scm"
+   output: (string-append (current-build-directory) lib-name lib-suffix)))
+
+(define-task install (compile)
+  (make-directory lib-directory)
+  (delete-file (string-append lib-directory lib-name lib-suffix))
+  (copy-file (string-append (current-build-directory) lib-name lib-suffix)
+             (string-append lib-directory lib-name lib-suffix)))
+
+(define-task all (compile install)
+  '(compile and install))
