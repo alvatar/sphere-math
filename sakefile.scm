ff4596f7f81040fe593ead8f039f08e3f2b2b240
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-10-23 18:04:01 +0200

    refactored sake: to sake#

--- sakefile.scm
+++ sakefile.scm
@@ -4,23 +4,23 @@
     matrix))
 
 (define-task clean ()
-  (sake:default-clean))
+  (sake#default-clean))
 
 (define-task compile ()
   (for-each (lambda (m)
-              (sake:compile-c-to-o (sake:compile-to-c m))
-              (sake:compile-c-to-o (sake:compile-to-c
+              (sake#compile-c-to-o (sake#compile-to-c m))
+              (sake#compile-c-to-o (sake#compile-to-c
                                     m
                                     version: '(debug)
                                     compiler-options: '(debug))))
             modules))
 
 (define-task install ()
-  (for-each (lambda (m) (sake:install-compiled-module m versions: '(() (debug)))) modules)
-  (sake:install-sphere-to-system))
+  (for-each (lambda (m) (sake#install-compiled-module m versions: '(() (debug)))) modules)
+  (sake#install-sphere-to-system))
 
 (define-task uninstall ()
-  (sake:uninstall-sphere-from-system))
+  (sake#uninstall-sphere-from-system))
 
 (define-task all (compile install)
   'all)
