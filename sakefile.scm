ca00578b3bcaeee060c2c7616ca241fb8c8b0798
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-12-28 16:39:37 +0100

    unified sakefiles

--- sakefile.scm
+++ sakefile.scm
@@ -3,20 +3,21 @@
     bit-logical
     matrix))
 
+(define-task compile ()
+  (for-each (lambda (m) (sake#compile-module m compiler-options: '(debug))) modules)
+  (for-each sake#compile-module modules))
+
+(define-task post-compile ()
+  (for-each (lambda (m) (sake#make-module-available m versions: '(() (debug)))) modules))
+
+(define-task install ()
+  (sake#install-sphere-to-system))
+
+(define-task test ()
+  (sake#test-all))
+
 (define-task clean ()
   (sake#default-clean))
 
-(define-task compile ()
-  (for-each (lambda (m)
-              (sake#compile-module m)
-              (sake#compile-module m compiler-options: '(debug)))
-            modules))
-
-(define-task install ()
-  (for-each (lambda (m) (sake#install-compiled-module m versions: '(() (debug)))) modules))
-
-(define-task force-install ()
-  (sake#install-sphere-to-system))
-
-(define-task all (compile install)
+(define-task all (compile post-compile)
   'all)
